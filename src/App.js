33cdb1d7804d9e66d2782bd291bd53edbe75517b
benndip <obentabiayuk1@gmail.com>
2021-05-11 13:09:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+/* eslint-disable new-parens */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect, useRef } from 'react';
+import * as faceapi from 'face-api.js'
 import './App.css';
 
-function App() {
+const App = () => {
+  const videoWidth = 720;
+  const videoHeight = 560;
+  const [initializing, setInitializing] = useState(false)
+  const videoRef = useRef()
+  const canvasRef = useRef()
+
+  const handleVideoOnplay = () => {
+    setInterval(async () => {
+      if (initializing) {
+        setInitializing(false)
+      }
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current)
+      const disPlaySize = {
+        width: videoWidth,
+        height: videoHeight
+      }
+      faceapi.matchDimensions(canvasRef.current, disPlaySize);
+      const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, disPlaySize);
+      canvasRef.current.getContext("2d").fillRect(0, 0, videoWidth, videoHeight)
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections)
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections)
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections)
+      console.log(detections)
+    }, 100);
+  }
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + '/models'
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo)
+    }
+    loadModels();
+  }, [])
+
+  const startVideo = () => {
+    navigator.mediaDevices.getUserMedia({ video: {} }).then((stream) => { videoRef.current.srcObject = stream; }, (err) => console.error(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{initializing ? 'Initializing' : 'Ready'}</span>
+      <div className="video-and-canva">
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          height={videoHeight}
+          width={videoWidth}
+          onPlay={handleVideoOnplay}
+        />
+        <canvas ref={canvasRef} className="canva" />
+      </div>
     </div>
   );
 }
