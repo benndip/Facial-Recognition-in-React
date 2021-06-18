9d20d98e7f7556a5b9d66eb097f63623016fe1d9
Queen <pro-3is@Pro-3s-MacBook-Air.local>
2021-06-18 08:35:15 +0100

    the model now recognises faces and display the name of the face it captured

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,20 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
 
 const Login = () => {
-    return(
-        <div>
-            <form method="post">
-                <p>Register</p>
-                Name: <input />
-                Email: <input />
-                Password: <input />
-                <button type="submit">Submit</button>
-                <p>Already have an account? SIGN IN</p>
-            </form>
-        </div>
-    )
+
+    return (
+        <div></div>
+    );
 
 }
 export default Login;
